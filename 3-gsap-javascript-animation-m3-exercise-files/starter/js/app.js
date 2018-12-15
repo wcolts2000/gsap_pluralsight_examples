02e14bfb4236b7e59d2f42267a4c65fb26f2e1c3
wcolts2000 <wcolts2000@gmail.com>
2018-12-15 15:56:09 -0800

    buttons added to timeline to make it operate on clicks

--- 3-gsap-javascript-animation-m3-exercise-files/starter/js/app.js
+++ 3-gsap-javascript-animation-m3-exercise-files/starter/js/app.js
@@ -1,8 +1,37 @@
-$(document).ready(function(){
-    init();
+$(document).ready(function() {
+  init();
 });
 
+function init() {
+  let toolTimeline = new TimelineLite({ paused: true });
+  let duration = 1;
 
-function init(){
+  toolTimeline.add(
+    TweenLite.to("#toolBox", duration, { y: -100, ease: Linear.easeInOut })
+  );
+  toolTimeline.add(
+    TweenLite.to("#weightLifter", duration, { y: -100, ease: Bounce.easeInOut })
+  );
+  toolTimeline.add(
+    TweenLite.to("#crazy", duration, { y: -100, ease: Elastic.easeInOut })
+  );
+  toolTimeline.add(
+    TweenLite.to("#leaf", duration, { y: -100, ease: Back.easeInOut })
+  );
 
+  $("#start").click(function() {
+    toolTimeline.play();
+  });
+
+  $("#pause").click(function() {
+    toolTimeline.pause();
+  });
+
+  $("#stop").click(function() {
+    toolTimeline.stop();
+  });
+
+  $("#reverse").click(function() {
+    toolTimeline.reverse();
+  });
 }
